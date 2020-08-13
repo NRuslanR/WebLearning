7e7ae1c29eea5ee0b8967d9bb54e257f1cec3db3
Ruslan Radikovich <nruslanr95@gmail.com>
2020-08-13 12:19:37 +0300

    AngularJS Event Handling

---
+++ AngularJS/Event Handling/QuestionController.js
@@ -0,0 +1,56 @@
+var testApp = angular.module('testApp');
+
+testApp.controller('questionController', function ($scope) {
+	
+	$scope.question = {
+		
+		author: 'Joschia Bounschi',
+		text: 'What is JS UI-Framework use?',
+		date: '2020-08-13',
+		answers: [
+			
+			{
+				author: 'John Witneg',
+				text: 'In my opinion, it would be better to use AngularJS because it is the most popular mean and it has great documentation',
+				date: '2020-08-14',
+				rate: 2
+			},
+			
+			{
+				author: 'Tim Rodney',
+				text: 'I am prefer more the ExtJs',
+				date: '2020-08-14',
+				rate: 1
+			},
+			
+			{
+				author: 'Sam Wildson',
+				text: 'I am using the React JS framework for more than 3 years and i am great with him',
+				date: '2020-08-15',
+				rate: 0
+			}
+		
+		]
+		
+	};
+	
+	$scope.voteUp = function (answer) {
+		
+		++answer.rate;
+		
+	}
+	
+	$scope.voteDown = function (answer) {
+		
+		--answer.rate;
+		
+	}
+	
+	$scope.questionColorClass = "questionBlock";
+	$scope.changeQuestionColorClass = function (e) {
+		
+		$scope.questionColor = (e.type === "mouseover") ? "questionSelectedBlock" : 'questionBlock';
+		
+	}
+	
+});
