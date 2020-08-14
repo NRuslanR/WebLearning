df0cbc048442e6009a1db8820ce3f89758473f43
Ruslan Radikovich <nruslanr95@gmail.com>
2020-08-14 14:34:59 +0300

    AngularJS - $http configuring: url, headers, params, method, timeout

--- AngularJS/Services/js/questionHTTPService.js
+++ AngularJS/Services/js/questionHTTPService.js
@@ -1,23 +1,39 @@
 questionApp.factory/*service*/('questionService', function ($http) {
 
+	function logResponseInfo(response)
+	{
+		console.log('status: ' + response.status);
+		console.log('headers: ' + response.headers);
+		console.log('statusText: ' + response.statusText);
+		console.log('url: ' + response.config.url);
+	}
+		
 	return {
 			
 		getQuestionDataAsync: function(onSuccess, onFail) {
 				
+			//$http.get('http://localhost/questionApp/question.json')
 			$http({
 				method: 'GET',
-				url: 'http://localhost/questionApp/question.json'
+				url: 'http://localhost/questionApp/question.json',
+				timeout: 600,
+				params: { question_id: null },
+				headers: { 'Content-Type': 'application/json' }
 			})
 			.then(
 					
 				function (response) {
 							
+					logResponseInfo(response);
+					
 					onSuccess(response.data);
 							
 				},
 						
 				function (response) {
-							
+						
+					logResponseInfo(response);
+					
 					onFail(response.statusText);
 							
 				}
