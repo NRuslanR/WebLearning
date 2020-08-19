ce4353731408c7c8e8bea2154be3f125c0c98b11
Ruslan Radikovich <nruslanr95@gmail.com>
2020-08-19 18:39:33 +0300

    NodeJs - Application parameters: process.argv

---
+++ NodeJS/Modules/AppParams/app.js
@@ -0,0 +1,9 @@
+var nodePath = process.argv[0],
+    appPath = process.argv[1],
+    userName = process.argv[2],
+    userAge = process.argv[3];
+
+console.log(`node path: ${nodePath}`)
+console.log(`app path: ${appPath}`)
+console.log(`user name: ${userName}`)
+console.log(`user age: ${userAge}`);
