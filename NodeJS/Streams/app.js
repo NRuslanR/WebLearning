3c4d162e2fc389ce9f930fb560f33ae12156c789
Ruslan Radikovich <nruslanr95@gmail.com>
2020-08-20 12:19:23 +0300

    NodeJS - Streams: file streams, createWriteStream, createReadStream, write, end, on

---
+++ NodeJS/Streams/app.js
@@ -0,0 +1,34 @@
+const fs = require("fs");
+
+let writeableStream = fs.createWriteStream("test.txt");
+
+writeableStream.on('end', function () {
+
+  console.log('end has executed');
+
+});
+
+writeableStream.write('First suggestion');
+writeableStream.write('Second suggestion');
+writeableStream.end('Third suggestion');
+
+let readableStream = fs.createReadStream("test.txt");
+
+readableStream
+  .on('data', function (data) {
+
+    console.log('data chunk was arrived: ' + data);
+
+  })
+  .on('end', function () {
+
+    console.log('all data chunks were arrived');
+
+  })
+  .on("error", function (error) {
+
+    console.log("Error occurred during the file data read");
+
+  });
+
+fs.unlinkSync("test.txt");
