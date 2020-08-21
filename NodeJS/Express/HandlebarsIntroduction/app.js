e6b286ea728f36737680e70b34fd94f536d86cf9
Ruslan Radikovich <nruslanr95@gmail.com>
2020-08-21 10:16:24 +0300

    Node Express - Handlebars View Engine: app.set('view engine', 'handlebars'), app.set('views', '...')

---
+++ NodeJS/Express/HandlebarsIntroduction/app.js
@@ -0,0 +1,36 @@
+const express = require("express"),
+      app = express();
+
+app.set('view engine', 'hbs');
+app.set('views', 'views');
+
+app
+  .get('/contacts/:emailsVisible?', (req, res) => {
+
+    let emailsVisible = req.params['emailsVisible'];
+
+    if (!emailsVisible) {
+
+      emailsVisible = 1;
+
+      console.log('emailsVisible was not specified.');
+
+    }
+
+    res.render(
+        "contacts.hbs",
+        {
+          title: "My Contacts",
+          emailsVisible: emailsVisible > 0,
+          emails: [ 'brainsort@yandex.ru', 'nruslanr95@gmail.com' ],
+          phone: '+7 123 456 78 91'
+        }
+    );
+
+  })
+  .get('/', (req, res) => {
+
+    res.send('<a href="/contacts">Contacts</a>');
+
+  })
+  .listen(3000);
