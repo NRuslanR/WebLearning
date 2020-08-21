7aed4f4e1c63f051336146a587f95e7d264477bd
Ruslan Radikovich <nruslanr95@gmail.com>
2020-08-21 14:58:32 +0300

    Node Express - View Templating Engine Pug: app.set('view engine', 'pug')

---
+++ NodeJS/Express/Pug/app.js
@@ -0,0 +1,47 @@
+const express = require("express"),
+      app = express();
+
+app.set('view engine', 'pug');
+
+app
+  .get('/contacts/:emailsVisible?', (req, res) => {
+
+    let emailsVisible = req.params['emailsVisible'];
+
+    if (!emailsVisible) {
+
+      emailsVisible = 1;
+
+      console.log('emailsVisible was not specified.');
+
+    }
+
+    res.render(
+        "contacts",
+        {
+          title: "My Contacts",
+          emailsVisible: emailsVisible > 0,
+          emails: [ 'brainsort@yandex.ru', 'nruslanr95@gmail.com' ],
+          phone: '+7 123 456 78 91'
+        }
+    );
+
+  })
+  .get('/', (req, res) => {
+
+    res.render(
+      'home',
+      {
+        title: 'Home',
+        frameworks: [
+          'Node.js',
+          'Express',
+          'AngularJS',
+          'Vue.js',
+          'React.js'
+        ]
+      }
+    );
+
+  })
+  .listen(3000);
